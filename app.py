c7a0c4872565cc1bc1f2643639780e6778057ca4
Aimnor <m.fabre123@gmail.com>
2025-06-29 21:35:19 +0200

    [WIP]

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from requests import request
+from source import BeerAgenda
+
+app = Flask(__name__)
+
+ba = BeerAgenda(force_get_event=False, all_professionals=True)
+
+@app.route('/professionals',methods = ['GET'])
+def professionals():
+    return [pro.to_dict(True) for pro in ba.professionals]
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
