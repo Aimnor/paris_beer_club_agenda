2a59397aa05bad462e805b3b802038c56b82df30
Aimnor <m.fabre123@gmail.com>
2025-06-29 17:46:37 +0200

    WIP

--- data/extract.py
+++ data/extract.py
@@ -1,6 +1,15 @@
 import json
+
 with open("data/subscribers.json", "r") as file_handler:
-    data = json.load(file_handler)
+    subs = [sub["relative_url"] for sub in json.load(file_handler)]
     
-with open("data/subscribers.txt", "w") as file_handler:
-    file_handler.writelines([subscriber["relative_url"]+"\n" for subscriber in data])
+with open("data/all_time_subscribers.json", "r") as file_handler:
+    pros = json.load(file_handler)
+
+for pro in pros:
+    pro["subscribed"] = False
+    if pro["relative_url"] in subs:
+        pro["subscribed"] = True
+
+with open("data/professionals.json", "w") as file_handler:
+    json.dump(pros, file_handler, indent=4, ensure_ascii=False)
